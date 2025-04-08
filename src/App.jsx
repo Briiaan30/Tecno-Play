cdaf18c8be700e33737076dc0476bdd3aad564eb
briian30 <briansarmiento.93@gmail.com>
2025-04-08 09:18:42 -0300

    Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { StrictMode } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import ResponsiveAppBar from './components/Navbar/Navbar.jsx'
+import ResponsiveAppBar from './components/Navbarr/Navbar.jsx'
 import ItemListContainer from './components/ItemListContainer/ItemListContainer.jsx'
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer.jsx'
 import CantidadCarrito from './components/CantidadCarrito/CantidadCarrito.jsx'
 import { CartComponentContext } from './context/CartContexto.jsx'
-import Cart from './components/Cart/Cart.jsx'
+// import ItemCount from './components/ItemCount/ItemCount.jsx'
+// import ComponentTests from './components/ComponentTests/ComponentTests.jsx'
 
 
 function App() {
@@ -25,7 +26,7 @@ function App() {
             <Route exact path="/" element={<ItemListContainer />} />
             <Route path="/category/:categoryId" element={<ItemListContainer />} />
             <Route path="/item/:id" element={<ItemDetailContainer />} />
-            <Route path="/cart" element={<Cart />} />
+            <Route path="/cart" element={<CantidadCarrito />} />
           </Routes>
         </BrowserRouter>
       </CartComponentContext>
